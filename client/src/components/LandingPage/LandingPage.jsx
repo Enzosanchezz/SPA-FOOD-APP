8e396bd9b4cae9cd8e131563fdcf5ceacb9f47e6
Enzosanchezz <enzosanchez771@gmail.com>
2022-08-21 03:23:53 -0300

    detail

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -7,12 +7,11 @@ export default function LandingPage(){
         <div>
             <span className={style.landing}>
                 <h1>Bienvendidos! 😊</h1>
-                <h3>Hola mi nombre es Enzo 👋🏻, antes de clickear el enlace "Comenzar" déjame contarte un poco de lo que se trata este proyecto.</h3>
-                <h4>Es una app sobre recetas de comidas traidas desde una api externa y almacenadas en una base de datos.
-                    El objetivo era armar el back-end y el front-end, conectarlos y darle un poco de estilo con css.
-                    Ahora que ya sabes mas o menos de que se trata, cliclea 👉🏻 <Link className={style.link} to="/home">
-                    Comenzar
-                </Link> y disfruta la app!! 
+                <h3>Hola mi nombre es Enzo 👋🏻, antes de clickear en "Comenzar" déjame contarte un poco de lo que se trata este proyecto.</h3>
+                <h4>Desarrolle una SPA(Single Page Application) utilizando React para el Front End y Redux cómo stmanagement. Todos los componentes fueron desarrollados con CSS sin uso de librerías externas. 
+                La SPA consume datos de una API a través de un Back End desarrollado en Node.JS utilizando Express, agregando nuevas funcionalidades a la API original.
+                Algunos features del proyecto: ordenamientos, filtros, busquedas, formulario controlado para la creación de Recetas, formulario controlado para actualizar Recetas.
+                Ahora si clickea <Link className={style.link} to="/home">Comenzar</Link> y disfruta la app!! 
                 </h4>
                 
             </span>
